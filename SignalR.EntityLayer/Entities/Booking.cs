b8f55728e31ad02ea9285146450868e52c6cbe30
msonmez25 <108278647+msonmez25@users.noreply.github.com>
2025-06-16 16:29:02 +0300

    Add project files.

---
+++ SignalR.EntityLayer/Entities/Booking.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        public int PersonCount { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ SignalR.EntityLayer/Entities/OpenInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class OpenInfo
+    {
+        public int OpenInfoID { get; set; }
+        public string Title { get; set; }
+        public DateTime DateFirst { get; set; }
+        public DateTime DateEnd { get; set; }
+    }
+}
