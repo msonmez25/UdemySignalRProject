bc2ea3487733768c17a64ed58353391259d8bf62
msonmez25 <108278647+msonmez25@users.noreply.github.com>
2025-10-17 15:13:04 +0300

    anasayfa navbar güncellemeleri

--- SignalRWebUI/Controllers/CustomerTableController.cs
+++ SignalRWebUI/Controllers/CustomerTableController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SignalRWebUI.Dtos.RestaurantTableDtos;
 
 namespace SignalRWebUI.Controllers
 {
+    [AllowAnonymous]
     public class CustomerTableController : Controller
     {
 
